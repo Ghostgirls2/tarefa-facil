f7ffc2873c9874880b7e62e6846e3dab9799c94d
Ghost_S2 <ghostgirll17.s2@gmail.com>
2025-04-09 19:39:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Função para simular o pagamento com Pix
-function pagarPix() {
-    alert("Gerando QR Code para pagamento via Pix...");
-    // Aqui você pode integrar a funcionalidade do Pix
-    // Exemplo: mostrar um QR Code do Pix ou enviar o usuário para uma página de pagamento Pix
-}
